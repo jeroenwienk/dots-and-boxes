b4c6ee14be2b324a94923b566fbfb635b93e6e9e
Jeroen Wienk <jeroenwienk@gmail.com>
2020-06-09 15:35:28 +0200

    url fix

---
+++ precache-manifest.f43d31c372a840558830b5ff5f10dccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71181159defbafda7ad7b4d8c3cccf38",
+    "url": "/dots-and-boxes/index.html"
+  },
+  {
+    "revision": "e753b82b7052ed88d13c",
+    "url": "/dots-and-boxes/static/css/main.45e94a50.chunk.css"
+  },
+  {
+    "revision": "da18257791430450e7fa",
+    "url": "/dots-and-boxes/static/js/2.7a824019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dots-and-boxes/static/js/2.7a824019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e753b82b7052ed88d13c",
+    "url": "/dots-and-boxes/static/js/main.a4299d75.chunk.js"
+  },
+  {
+    "revision": "34b367faf61236f2f2bc",
+    "url": "/dots-and-boxes/static/js/runtime-main.bcc0da9c.js"
+  }
+]);
