ec3fd3406d7adf72895a049a40f468c28ac39e22
Jeroen Wienk <jeroenwienk@gmail.com>
2020-06-09 15:13:24 +0200

    build

---
+++ build/precache-manifest.b5b01d2a47b47b823c5ceb4d11c08410.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45711564d256f3486ef3d2b7e030bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5c48cdb4ad6acdae001",
+    "url": "/static/css/main.082dec7b.chunk.css"
+  },
+  {
+    "revision": "da18257791430450e7fa",
+    "url": "/static/js/2.7a824019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a824019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5c48cdb4ad6acdae001",
+    "url": "/static/js/main.a4299d75.chunk.js"
+  },
+  {
+    "revision": "8496c3e9e85d1e17f9b2",
+    "url": "/static/js/runtime-main.fca8feef.js"
+  }
+]);
